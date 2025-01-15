0abfe5e2e880da46bcd29318c08cad10aee30b86
moluoxixi <10811655+moluoxixi@user.noreply.gitee.com>
2025-01-15 12:03:21 +0800

    refactor: 抽离所有storie,方便接入vitepress

--- stylelint.config.js
+++ stylelint.config.js
@@ -5,9 +5,13 @@ export default {
     'stylelint-config-standard-less',
     'stylelint-config-standard-vue',
   ],
+  // 自定义的规则，覆盖默认的规则
   rules: {
-    // 在这里可以自定义的规则，覆盖默认的规则
+    // 禁用 是否应该满足小驼峰 规则
     'selector-class-pattern': null,
+    // 禁用 双斜杠注释不应该有空格 规则
+    'scss/double-slash-comment-whitespace-inside': null,
+    // 使 deep 可用
     'selector-pseudo-class-no-unknown': [
       true,
       {
