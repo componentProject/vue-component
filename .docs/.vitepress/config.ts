0ea47febf6092ecc71ce492b8a9956c96c4f4107
wanglun <wrnm.@.com>
2025-01-24 21:40:28 +0800

    perf: 测试发布

--- .docs/.vitepress/config.ts
+++ .docs/.vitepress/config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   title: 'vueComponent',
   description: '一个vue组件库',
   base: '/vue-component/',
+  lang: 'zh-CN',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     // 标题
