ad08109cd8de95e256547bb45abb857df04b9116
wanglun <wrnm.@.com>
2025-01-25 12:03:25 +0800

    perf: 修改流水线

--- deploy-storybook.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd .vitepress/dist
-git init
-git remote add origin https://github.com/componentProject/vue-component-storybook.git
-git add .
-git commit -m 'test'
-git push -u origin main
