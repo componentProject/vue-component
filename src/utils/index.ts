35f4d5e3d913142c86d79f0eee951e6fa4b1c6fe
moluoxixi <1983531544@qq.com>
2025-03-10 16:02:43 +0800

    perf: 测试eslint

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,12 @@
-export function cloneDeep(obj: any, map = new WeakMap()) {
+/**
+ * 深拷贝对象
+ * @param obj 需要拷贝的对象
+ * @param map WeakMap用于存储已经拷贝的对象，以便在遇到循环引用的情况时，直接返回缓存中的拷贝
+ * @returns 拷贝后的对象
+ */
+export function cloneDeep(obj: any, map = new WeakMap()): any {
   if (typeof obj !== 'object' || obj === null) {
+    // 如果不是对象，直接返回
     return obj
   }
 
@@ -15,6 +22,7 @@ export function cloneDeep(obj: any, map = new WeakMap()) {
     clone = []
     map.set(obj, clone)
     obj.forEach((item, index) => {
+      // 递归拷贝数组中的每个元素
       clone[index] = cloneDeep(item, map)
     })
   } else {
@@ -22,6 +30,7 @@ export function cloneDeep(obj: any, map = new WeakMap()) {
     map.set(obj, clone)
     for (const key in obj) {
       if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        // 递归拷贝对象中的每个属性
         clone[key] = cloneDeep(obj[key], map)
       }
     }
@@ -29,6 +38,7 @@ export function cloneDeep(obj: any, map = new WeakMap()) {
 
   // 处理函数类型
   if (typeof obj === 'function') {
+    // 将函数转换为字符串，并将其设置到拷贝对象中
     clone[typeof obj] = obj.toString()
   }
 
