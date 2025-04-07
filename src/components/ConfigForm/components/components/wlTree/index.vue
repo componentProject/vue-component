93688835cde894e2fa691e0a572fd3b4b19d1fc3
moluoxixi <1983531544@qq.com>
2025-04-07 17:50:02 +0800

    perf: 修改

--- src/components/ConfigForm/components/components/wlTree/index.vue
+++ src/components/ConfigForm/components/components/wlTree/index.vue
@@ -1,63 +1,42 @@
 <template>
-  <el-tree v-if="show" v-bind="Options" v-on="Event">
-    <!-- default 自定义树节点的内容，参数为 { node, data },分别为当前节点的 Node 对象和数据 -->
-    <template v-if="slots.default" #default="scope">
-      <slot name="default" v-bind="scope"></slot>
-    </template>
-  </el-tree>
+  <el-tree v-if="show" v-bind="Options" v-on="Event" />
 </template>
 
-<script lang="js">
+<script setup lang="ts">
+import { ref, watch } from 'vue'
 import { isType } from '../../utils'
-import { defineComponent } from 'vue'
-export default defineComponent({
-  name: 'wlTree',
-  props: {
-    prop: {
-      type: String,
-      default: '',
-    },
-    slots: {
-      type: Object,
-      default: () => {
-        return {}
-      },
-    },
-    model: {
-      type: Object,
-      default: () => {
-        return {}
-      },
-    },
-    config: {
-      type: Object,
-      default: () => {
-        return {}
-      },
-    },
+
+const props = withDefaults(
+  defineProps<{
+    prop: string
+    slots: Record<string, any>
+    model: Record<string, any>
+    config: Record<string, any>
+  }>(),
+  {
+    prop: '',
+    slots: () => ({}),
+    model: () => ({}),
+    config: () => ({}),
   },
-  data() {
-    return {
-      show: true,
-      Event: {},
-      Options: {},
+)
+
+const show = ref(true)
+const Event = ref({})
+const Options = ref({})
+
+watch(
+  () => props.config,
+  (v) => {
+    const { show: showVal, event, ...rest } = v
+    if (isType(showVal, 'boolean')) {
+      show.value = !!showVal
     }
+    Options.value = rest
+    Event.value = event || {}
   },
-  watch: {
-    config: {
-      handler(v) {
-        const { show, event, ...Options } = v
-        if (isType(show, 'boolean')) {
-          this.show = !!show
-        }
-        this.Options = Options
-        this.Event = event || {}
-      },
-      immediate: true,
-      deep: true,
-    },
-  },
-})
+  { immediate: true, deep: true },
+)
 </script>
 
 <style scoped lang="scss"></style>
