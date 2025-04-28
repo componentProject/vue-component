8885e24f2732f6291de14ba35be7e9cc8c4f9cbb
moluoxixi <1983531544@qq.com>
2025-04-28 21:35:55 +0800

    feat: 一群的导出工作

---
+++ .storybook/.stories/excelTotal.tsx
@@ -0,0 +1,22 @@
+import excelTotal from '@/components/excelTotal/index.vue'
+import type { Meta, StoryFn } from '@storybook/vue3'
+const meta: Meta<any> = {
+  title: 'excelTotal',
+  component: excelTotal,
+  args: {},
+  argTypes: {},
+}
+export default meta
+export const excelTotalDemo: StoryFn = (args: any) => ({
+  template: `
+    <div><excelTotal /> </div>
+  `,
+  components: { excelTotal },
+  setup() {
+    return {
+      ...args,
+    }
+  },
+})
+const props = {}
+excelTotalDemo.args = props
