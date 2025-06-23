61370a0f5d1fd604a51f9694d6da7a9cbfec32d0
moluoxixi <1983531544@qq.com>
2025-06-23 19:17:55 +0800

    feat: 删除低劣组件

--- .storybook/.stories/ConfigProvider.stories.tsx
+++ .storybook/.stories/ConfigProvider.stories.tsx
@@ -3,39 +3,15 @@ import type { propsType } from '@/components/ConfigProvider/types'
 import type { Meta, StoryObj } from '@storybook/vue3'
 
 import Calendar from '@/components/Calendar/index.vue'
-import Space from '@/components/Space/index.vue'
-import './assets/styles/Space.css'
 
 const meta: Meta<propsType> = {
   title: 'ConfigProvider',
   component: ConfigProvider,
   args: {},
-  tags: ['!autodocs']
+  tags: ['!autodocs'],
 }
 export default meta
 //#region 直接args传值,好处是docs里示例代码正常
-export const space: StoryObj = {
-  args: {
-    space: 'small',
-    // 默认插槽
-    default: () => (
-      <Space>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
-      </Space>
-    ),
-  },
-  argTypes: {
-    default: {
-      control: false,
-    },
-    space: {
-      control: 'radio',
-      options: ['small', 'middle', 'large', 32],
-    },
-  },
-}
 export const calendar: StoryObj = {
   args: {
     locale: 'zh-CN',
@@ -70,28 +46,6 @@ export const calendar: StoryObj = {
 //   locale: {
 //     control: 'radio',
 //     options: ['zh-CN', 'en-US']
-//   }
-// }
-//
-// export const space = (args) => ({
-//   template: `
-//     <ConfigProvider :space="args.space">
-//       <Space>
-//         <div class="box"></div>
-//         <div class="box"></div>
-//         <div class="box"></div>
-//       </Space>
-//     </ConfigProvider>
-//   `,
-//   components: { ConfigProvider, Space },
-//   setup () {
-//     return { args }
-//   }
-// })
-// space.argTypes = {
-//   space: {
-//     control: 'radio',
-//     options: ['small', 'middle', 'large', 32]
 //   }
 // }
 //#endregion
