fbdb05f354ee90daead8d79d649582b68fd14e21
moluoxixi <1983531544@qq.com>
2025-08-26 12:21:10 +0800

    feat: utils抽离

--- play/render/vite.config.ts
+++ play/render/vite.config.ts
@@ -1,4 +1,4 @@
-import viteConfig, { wrapperEnv } from '../../packages/components//ViteConfig/index.ts'
+import viteConfig, { wrapperEnv } from '../../packages/utils/ViteConfig/index.ts'
 import path from 'node:path'
 import process from 'node:process'
 import { loadEnv } from 'vite'
