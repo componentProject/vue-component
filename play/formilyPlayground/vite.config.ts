7666705183595382a5762c90f18903bc6132dd3f
moluoxixi <1983531544@qq.com>
2025-09-09 14:01:12 +0800

    feat: 3834 bug修复与代码优化

--- play/formilyPlayground/vite.config.ts
+++ play/formilyPlayground/vite.config.ts
@@ -1,22 +1,29 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import viteConfig, { wrapperEnv } from '../../packages/utils/ViteConfig/index.ts'
+import path from 'node:path'
+import process from 'node:process'
+import { loadEnv } from 'vite'
 
-export default defineConfig({
-  base: '/element-plus',
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      // ...getAlias('@moluoxixi/element'),
-      // ...getAlias('@moluoxixi/element-prototypes'),
-      // ...getAlias('@moluoxixi/element-renderer'),
-      // ...getAlias('@moluoxixi/element-setters'),
-      // ...getAlias('@moluoxixi/element-settings-form'),
-    },
+export default viteConfig(
+  ({ mode }) => {
+    const env = loadEnv(mode!, process.cwd())
+    const viteEnv = wrapperEnv(env)
+    const rootPath = path.resolve()
+    return {
+      rootPath,
+      mode: {
+        base: {
+          VITE_AUTO_ROUTES: true,
+          VITE_GLOB_APP_TITLE: viteEnv.VITE_GLOB_APP_TITLE,
+          VITE_GLOB_APP_CODE: viteEnv.VITE_GLOB_APP_CODE,
+          VITE_PORT: 3400,
+          VITE_OPEN: true,
+          VITE_COMPRESS: true,
+          VITE_IMAGEMIN: true,
+          VITE_BUILD_GZIP: true,
+        },
+        development: {},
+        production: {},
+      },
+    }
   },
-  server: {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-  },
-})
+)
