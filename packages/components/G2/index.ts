311a351122795595ee8da080f06b6709a37b46ed
moluoxixi <1983531544@qq.com>
2025-10-11 16:30:05 +0800

    feat: 4256 G2打包

--- packages/components/G2/index.ts
+++ packages/components/G2/index.ts
@@ -1,5 +1,8 @@
-import { Chart } from '@antv/g2'
-
+// 等同extend(Runtime, stdlib())
+// import { Chart } from '@antv/g2'
+import { corelib, extend, Runtime } from '@antv/g2'
+// 按需打包，https://g2.antv.antgroup.com/manual/extra-topics/bundle
+const Chart = extend(Runtime, corelib())
 export {
   Chart,
 }
--- packages/components/G2/src/Example.vue
+++ packages/components/G2/src/Example.vue
@@ -1,24 +1,93 @@
 <template>
-  <div id="container" />
+  <div style="height: 20%;width: 30%">
+    <div id="container" class="w-full h-full" />
+  </div>
 </template>
 
 <script setup lang="ts">
-import { Chart } from '../index'
+// import { Chart } from '../index'
+import { Chart } from '@antv/g2'
+import { onBeforeUnmount, onMounted, ref } from 'vue'
 
-const chart = new Chart({ container: 'container' })
+const chart = ref()
+// 准备数据
+function renderBarChart(container) {
+  const data = [
+    { item: '事例一', count: 40, percent: 0.4 },
+    { item: '事例二', count: 21, percent: 0.21 },
+    { item: '事例三', count: 17, percent: 0.17 },
+    { item: '事例四', count: 13, percent: 0.13 },
+    { item: '事例五', count: 9, percent: 0.09 },
+  ]
 
-chart.options({
-  type: 'boxplot',
-  autoFit: true,
-  inset: 6,
-  data: {
-    type: 'fetch',
-    value: 'https://assets.antv.antgroup.com/g2/morley.json',
-  },
-  encode: { x: 'Expt', y: 'Speed' },
+  const chart = new Chart({
+    container: 'container',
+    autoFit: true,
+  })
+
+  chart.coordinate({ type: 'theta', outerRadius: 0.8, innerRadius: 0.5 })
+
+  chart
+    .interval()
+    .data(data)
+    .transform({ type: 'stackY' })
+    .encode('y', 'percent')
+    .encode('color', 'item')
+    .legend('color', { position: 'bottom', layout: { justifyContent: 'center' } })
+    .label({
+      position: 'outside',
+      text: data => `${data.item}: ${data.percent * 100}%`,
+    })
+    .tooltip(data => ({
+      name: data.item,
+      value: `${data.percent * 100}%`,
+    }))
+
+  chart
+    .text()
+    .style('text', '主机')
+    // Relative position
+    .style('x', '50%')
+    .style('y', '50%')
+    .style('dy', -25)
+    .style('fontSize', 34)
+    .style('fill', '#8c8c8c')
+    .style('textAlign', 'center')
+
+  chart
+    .text()
+    .style('text', '200')
+    // Relative position
+    .style('x', '50%')
+    .style('y', '50%')
+    .style('dx', -25)
+    .style('dy', 25)
+    .style('fontSize', 44)
+    .style('fill', '#8c8c8c')
+    .style('textAlign', 'center')
+
+  chart
+    .text()
+    .style('text', '台')
+    // Relative position
+    .style('x', '50%')
+    .style('y', '50%')
+    .style('dx', 35)
+    .style('dy', 25)
+    .style('fontSize', 34)
+    .style('fill', '#8c8c8c')
+    .style('textAlign', 'center')
+
+  chart.render()
+
+  return chart
+}
+onMounted(() => {
+  chart.value = renderBarChart('container')
 })
-
-chart.render()
+onBeforeUnmount(() => {
+  chart.value?.destroy()
+})
 </script>
 
 <style scoped>
