3ab55928ee20876b5ca52f9f122d89af95e5334a
moluoxixi <1983531544@qq.com>
2025-10-23 15:41:55 +0800

    feat: BUG修复

--- packages/components/PopoverTableSelect/src/index.vue
+++ packages/components/PopoverTableSelect/src/index.vue
@@ -154,10 +154,6 @@ function handleScrollBoundary(obj) {
 }
 </script>
 
-<style>
-@import 'element-plus/theme-chalk/el-input.css';
-</style>
-
 <style scoped lang="scss">
 @forward '@moluoxixi/components/_assets/styles/tailwind.scss';
 </style>
