185ee21a55e7c5dbfdace78652553966fa888325
moluoxixi <1983531544@qq.com>
2025-10-27 16:02:51 +0800

    feat: 添加批量组件调试

--- packages/components/Tabs/src/index.vue
+++ packages/components/Tabs/src/index.vue
@@ -6,22 +6,33 @@
       :class="props.type === 'border-card' && 'tabs-card'"
       @tab-change="handleTabChange"
     >
-      <template v-for="item in props.tabList">
-        <ElTabPane
-          v-if="item.show ? item.show(item) : true"
-          :key="item.label"
-          style="height: 100%"
-          v-bind="item"
-        >
-          <slot :name="item.slot || item.label" />
-        </ElTabPane>
-      </template>
+      <ElTabPane
+        v-for="item in filteredOptions"
+        :key="item[computedValue]"
+        :label="item[computedLabel]"
+        :name="item[computedValue]"
+        :disabled="
+          computedDisabledHandler({
+            label: item[computedLabel],
+            value: item[computedValue],
+            data: item,
+          })
+        "
+        style="height: 100%"
+        v-bind="item"
+      >
+        <slot name="default" :item="item" />
+        <slot :name="item.slot || item[computedLabel]" />
+      </ElTabPane>
     </ElTabs>
   </div>
 </template>
 
 <script setup lang="ts">
+import { computed, withDefaults } from 'vue'
 import { ElTabPane, ElTabs } from 'element-plus'
+import { getTypeDefault } from '@moluoxixi/utils/_utils'
+import { useOptions } from '../../_hooks'
 import type { emitsType, propsType, slotsType } from './_types'
 
 defineOptions({
@@ -32,6 +43,18 @@ defineOptions({
 const props = withDefaults(defineProps<propsType>(), {
   tabList: () => [],
   type: '',
+  // 新增的 options 模式默认值
+  options: () => [],
+  label: 'label',
+  value: 'name',
+  disabledValues: () => [],
+  disabledLabels: () => [],
+  requestMethod: 'POST',
+  requestUrl: '',
+  requestParams: () => ({}),
+  requestParamsType: 'body',
+  requestHeaders: () => ({}),
+  responseDataPath: '',
 })
 
 const emit = defineEmits<emitsType>()
@@ -40,6 +63,32 @@ const emit = defineEmits<emitsType>()
 const slots = defineSlots<slotsType>()
 
 const activeName = defineModel({ default: '0', type: String })
+
+// 计算字段名
+const computedLabel = computed(() => props.labelKey || props.label)
+const computedValue = computed(() => props.valueKey || props.value)
+
+// 使用 useOptions hook 来处理 options 获取逻辑
+const { options: serverOrLocalOptions } = useOptions(props)
+
+// 过滤 options
+const filteredOptions = computed(() => {
+  if (props.tabList?.length) {
+    return props.tabList?.filter(item => item.show ? item.show(item) : true) || []
+  }
+  else {
+    return serverOrLocalOptions.value.filter(item => item.show ? item.show(item) : true)
+  }
+})
+
+// 默认禁用处理函数
+function defaultDisabledHandler({ label, value }: { [label: string]: any }) {
+  return props.disabledValues.includes(value) || props.disabledLabels.includes(label)
+}
+
+const computedDisabledHandler = computed(() => {
+  return getTypeDefault(props.disabledHandler, 'function') || defaultDisabledHandler
+})
 
 function handleTabChange(val: any) {
   emit('tabChange', val)
@@ -56,12 +105,14 @@ function handleTabChange(val: any) {
 
 <style scoped lang="scss">
 @forward '@moluoxixi/components/_assets/styles/tailwind.scss';
+.el-tabs {
+  height: 100%;
+}
 .tabs-card {
   background: #fff;
   border-radius: 8px !important;
   overflow: hidden;
   box-shadow: 2px 2px 10px 0 rgb(52 81 212 / 20%);
-  height: 100%;
 
   &.no-shadow {
     box-shadow: none;
