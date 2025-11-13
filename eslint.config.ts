95ed3be42339ae51ce0e0e9d9a371fbafde7fe12
moluoxixi <1983531544@qq.com>
2025-11-13 12:10:33 +0800

    feat: eslint 规则修改

--- eslint.config.ts
+++ eslint.config.ts
@@ -6,5 +6,11 @@ export default EslintConfig(
       'docs/vitepress/.vitepress/theme/components/DocsCodeDemo/**',
       'packages/components/AIAgent/**',
     ],
+    rules: {
+      // 未使用的变量
+      'unused-imports/no-unused-vars': 'off',
+      // 全局process
+      'node/prefer-global/process': 'off',
+    },
   },
 )
--- packages/utils/EslintConfig/src/index.ts
+++ packages/utils/EslintConfig/src/index.ts
@@ -1,6 +1,6 @@
-import antfu from '@antfu/eslint-config'
-
 import type { optionsType, userConfigType } from './_types/index.ts'
+
+import antfu from '@antfu/eslint-config'
 import { deepMerge } from '@moluoxixi/utils/_utils'
 
 export default function createEslintConfig(config: optionsType, ...userConfigs: userConfigType[]) {
@@ -17,11 +17,16 @@ export default function createEslintConfig(config: optionsType, ...userConfigs: 
         ...ignores,
       ],
       rules: {
-        'style/spaced-comment': 'off',
-        //#region 是否强制使用三等号
-        'eqeqeq': 'off',
-        'vue/eqeqeq': 'off',
-        //#endregion
+        // 注释后是否强制一个空格
+        'style/spaced-comment': ['error', 'always', {
+          line: {
+            // markers 列表中的标记，可以紧跟在 // 后，无需空格
+            markers: [
+              '#region',
+              '#endregion',
+            ],
+          },
+        }],
         // vue 组件块顺序
         'vue/block-order': ['error', {
           order: ['template', 'script', 'style'],
@@ -29,21 +34,28 @@ export default function createEslintConfig(config: optionsType, ...userConfigs: 
         //#region 不能在定义前使用变量
         'no-use-before-define': 'off',
         'ts/no-use-before-define': 'off',
-        //#endregion
-        // import 排序
-        'perfectionist/sort-imports': 'off',
         // JSON 排序
         'jsonc/sort-keys': 'off',
         // 不能使用console
         'no-console': 'off',
-        // 未使用的变量
-        'unused-imports/no-unused-vars': 'off',
-        // 全局process
-        'node/prefer-global/process': 'off',
-        //#region yaml
-        'yaml/sort-keys': 'off',
-        'yaml/spaced-comment': 'off',
+        // // 未使用的变量
+        // 'unused-imports/no-unused-vars': 'off',
+        // // 全局process
+        // 'node/prefer-global/process': 'off',
+
+        // //#region 是否强制使用三等号
+        // 'eqeqeq': 'off',
+        // 'vue/eqeqeq': 'off',
+        // //#endregion
+
         //#endregion
+        // // import 排序
+        // 'perfectionist/sort-imports': 'off',
+
+        // #region yaml
+        // 'yaml/sort-keys': 'off',
+        // 'yaml/spaced-comment': 'off',
+        // #endregion
       },
     }, otherOptions),
     ...userConfigs,
