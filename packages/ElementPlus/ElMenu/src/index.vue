e498758c64b5a816b1a1a17d2b304e08aaf0bae5
moluoxixi <1983531544@qq.com>
2025-11-14 18:05:02 +0800

    feat: Element样式局部化进程之ElMenu

--- packages/ElementPlus/ElMenu/src/index.vue
+++ packages/ElementPlus/ElMenu/src/index.vue
@@ -1,17 +1,22 @@
 <template>
-  <!--  <div :class="cssModules.root"> -->
-  <ElMenu :class="cssModules.root">
-    <p class="a">
-      111111111111111
-    </p>
-    <slot />
-  </ElMenu>
-<!--  </div> -->
+  <div :class="cssModules.root" style="display: contents">
+    <ElMenu :class="cssModules.root">
+      <!-- 使用插槽方式渲染自定义内容 -->
+      <template v-for="name in slotNames" #[name]="slotParams" :key="name">
+        <slot :name="name" v-bind="slotParams || {}" />
+      </template>
+    </ElMenu>
+  </div>
 </template>
 
 <script setup lang="ts">
+import type { slotsType } from '@moluoxixi/element-plus/_types'
 import { ElMenu } from 'element-plus'
+import { computed } from 'vue'
 import cssModules from './styles/modules/index.module.scss'
+// 获取插槽
+const slots = defineSlots<slotsType>()
+const slotNames = computed<string[]>(() => Object.keys(slots) as string[])
 </script>
 
 <style scoped>
